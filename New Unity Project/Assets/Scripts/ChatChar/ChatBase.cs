ae1f99b20a9188066c080ab5412b44ca07ed58c5
stonk <stonksfarm37204@gmail.com>
2021-02-26 19:53:54 +0700

    edit code

--- New Unity Project/Assets/Scripts/ChatChar/ChatBase.cs
+++ New Unity Project/Assets/Scripts/ChatChar/ChatBase.cs
@@ -2,7 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "ChatCHR",menuName = "ChatCHR/Create a new ChatCHR")]
 public class ChatBase : ScriptableObject
 {
-    [SerializeField] string charactersName;
+    //[SerializeField] string charactersName;
+    
+    [SerializeField] Sprite characterSprite;
+    [SerializeField] Sprite chatSprite;
+    [SerializeField] CharactersName charactersName;
+    //[SerializeField] Text text;
+    [TextArea]
+    [SerializeField] string textStr;
+
+    ChatBase()
+    {
+    }
+
+    public string getName()
+    {
+        return textStr;
+    }
+
+
+}
+
+public enum CharactersName
+{
+    Kate,
+    Arm,
+    Gavin,
+    Pang,
+    GrandMa,
+    Dad,
+    Mom
 }
