25e1fe3c4d2f2ea370113b83888979fa4ee60b6e
stonk <stonksfarm37204@gmail.com>
2021-02-26 18:40:32 +0700

    first

---
+++ New Unity Project/Assets/Scripts/BG/onTapBg.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class onTapBg : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public void chatHome()
+    {
+        SceneManager.LoadScene("Chat");
+    }
+    private void FixedUpdate()
+    {
+        onTapDown();
+    }
+    void onTapDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            chatHome();
+            
+        }
+    }
+}
